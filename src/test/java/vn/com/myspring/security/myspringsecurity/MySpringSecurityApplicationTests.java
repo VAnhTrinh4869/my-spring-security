bab9c78e20d4cc8652a247ae94475c1e777b5e66
VanAnh <trinhvanh4869@gmail.com>
2023-01-31 22:22:11 +0700

    init commit

---
+++ src/test/java/vn/com/myspring/security/myspringsecurity/MySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.myspring.security.myspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
